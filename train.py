2e9d5f3667b34b50a1333c9eacf70802193e3d8b
Ayushee Mittal <ayusheemittal530@gmail.com>
2020-11-27 19:05:31 +0530

    Update train.py

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ import torch
 import torch.nn
 from Model import*
 from DataLoader import *
-from utils.torch_utils import select_device, init_seeds
+from utils.torch_utils import select_device, init_seeds, labels_to_class_weights
 import math
 
 mixed_precision = True
