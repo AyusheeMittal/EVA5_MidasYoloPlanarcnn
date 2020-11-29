60e2403105208e772b61599a72f6e4dfb2ce82a3
Ayushee Mittal <ayusheemittal530@gmail.com>
2020-11-29 14:44:46 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import json
 
 from torch.utils.data import DataLoader
 
-from models import *
-from utils.datasets import *
+from Models import *
+from Dataloader import *
 from utils.utils import *
 
 
@@ -50,9 +50,9 @@ def test(data,
 
     # Configure run
     #data = parse_data_cfg(data)
-    nc = 1 if single_cls else int(data['classes'])  # number of classes 4
-    path = data['valid']  # path to test images
-    names = load_classes(data['names'])  # class names ['hardhat', 'vest', 'mask', 'boots']
+    nc = 4 # 1 if single_cls else int(data['classes'])  # number of classes 4
+    path = "./data/customdata/custom_test.txt" #data['valid']  # path to test images
+    names = ['hardhat', 'vest', 'mask', 'boots'] #load_classes(data['names'])  # class names ['hardhat', 'vest', 'mask', 'boots']
     iouv = torch.linspace(0.5, 0.95, 10).to(device)  # iou vector for mAP@0.5:0.95
     iouv = iouv[0].view(1)  # comment for mAP@0.5:0.95
     niou = iouv.numel()
