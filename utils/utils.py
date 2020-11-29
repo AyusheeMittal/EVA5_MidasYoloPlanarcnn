3503f62447412b0c3a58cf63351ec4e68728bd76
Ayushee Mittal <ayusheemittal530@gmail.com>
2020-11-29 16:39:23 +0530

    updating plots_results definition

--- utils/utils.py
+++ utils/utils.py
@@ -224,3 +224,36 @@ def bbox_iou(box1, box2, x1y1x2y2=True, GIoU=False, DIoU=False, CIoU=False):
                 return iou - (rho2 / c2 + v * alpha)  # CIoU
 
     return iou
+
+def plot_results(start=0, stop=0, bucket='', id=()):  # from utils.utils import *; plot_results()
+    # Plot training 'results*.txt' as seen in https://github.com/ultralytics/yolov3#training
+    fig, ax = plt.subplots(2, 5, figsize=(12, 6))
+    ax = ax.ravel()
+    s = ['GIoU', 'Objectness', 'Classification', 'Precision', 'Recall',
+         'val GIoU', 'val Objectness', 'val Classification', 'mAP@0.5', 'F1']
+    if bucket:
+        os.system('rm -rf storage.googleapis.com')
+        files = ['https://storage.googleapis.com/%s/results%g.txt' % (bucket, x) for x in id]
+    else:
+        files = glob.glob('results*.txt') + glob.glob('../../Downloads/results*.txt')
+    for f in sorted(files):
+        try:
+            results = np.loadtxt(f, usecols=[2, 3, 4, 8, 9, 12, 13, 14, 10, 11], ndmin=2).T
+            n = results.shape[1]  # number of rows
+            x = range(start, min(stop, n) if stop else n)
+            for i in range(10):
+                y = results[i, x]
+                if i in [0, 1, 2, 5, 6, 7]:
+                    y[y == 0] = np.nan  # dont show zero loss values
+                    # y /= y[0]  # normalize
+                ax[i].plot(x, y, marker='.', label=Path(f).stem, linewidth=2, markersize=8)
+                ax[i].set_title(s[i])
+                if i in [5, 6, 7]:  # share train and val loss y axes
+                    ax[i].get_shared_y_axes().join(ax[i], ax[i - 5])
+        except:
+            print('Warning: Plotting error for %s, skipping file' % f)
+
+    fig.tight_layout()
+    ax[1].legend()
+    fig.savefig('results.png', dpi=200)
+    
