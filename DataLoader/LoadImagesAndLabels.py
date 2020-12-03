1e5301a8b6240c93be9d2af8073fcfe1982619d2
Ayushee Mittal <ayusheemittal530@gmail.com>
2020-12-04 02:24:54 +0530

    Update LoadImagesAndLabels.py

--- DataLoader/LoadImagesAndLabels.py
+++ DataLoader/LoadImagesAndLabels.py
@@ -248,7 +248,7 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
 
             # Letterbox
             shape = self.batch_shapes[self.batch[index]] if self.rect else self.img_size  # final letterboxed shape
-            img, ratio, pad, midas = letterbox(img, midas, shape, auto=False, scaleup=self.augment)
+            img, ratio, pad, midas = letterbox(img, shape, auto=False, scaleup=self.augment) #, midas
             shapes = (h0, w0), ((h / h0, w / w0), pad)  # for COCO mAP rescaling
 
             # Load labels
