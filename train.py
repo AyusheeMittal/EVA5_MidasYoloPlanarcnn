b8752262bd4c922982b820fd28f9645304c77626
Ayushee Mittal <ayusheemittal530@gmail.com>
2020-12-06 16:14:33 +0530

    Update train.py

--- train.py
+++ train.py
@@ -362,7 +362,7 @@ def train():
     n_burn = max(3 * nb, 500)  # burn-in iterations, max(3 epochs, 500 iterations)
     maps = np.zeros(nc)  # mAP per class
     # torch.autograd.set_detect_anomaly(True)
-    results = (0, 0, 0, 0, 0, 0, 0)  # 'P', 'R', 'mAP', 'F1', 'val GIoU', 'val Objectness', 'val Classification'
+    results = (0, 0, 0, 0, 0, 0, 0, 0)  # 'P', 'R', 'mAP', 'F1', 'val GIoU', 'val Objectness', 'val Classification'
     t0 = time.time()
     print('Image sizes %g - %g train, %g test' % (imgsz_min, imgsz_max, imgsz_test))
     print('Using %g dataloader workers' % nw)
@@ -415,6 +415,7 @@ def train():
             ssim_obj = SSIM()
             midas = midas.unsqueeze(1)
             ssim_loss = 1 - ssim_obj(pred[0], midas)
+            print('ssim_loss', ssim_loss)
             #ssim_loss_items = ssim_loss
             
             loss = lambda_y * yolo_loss + lambda_m * ssim_loss
